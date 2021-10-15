c2be4bc0c015d971d3648240b6707b9b4aafa5b7
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-15 19:24:22 -0400

    algumas implementações no card

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+
+import Card from './components/layout/Card'
 import Aleatorio from "./components/basicos/Aleatorio";
 import ComParametro from "./components/basicos/ComParametro";
 import Primeiro from "./components/basicos/Primeiro";
+import Fragmento from "./components/basicos/Fragmento";
 
-export default function App(props) {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
   return (
-    <div>
-      <Aleatorio min={1} max={60} />
-      {/* <Fragamento /> */}
-      <ComParametro titulo="Situação do Aluno" aluno="Pedro" nota={9.3}  />
-      <Primeiro />
+    <div id="App">
+      <h1>Fundamentos React</h1>
+      <Card titulo="#04 - Desafio aleatório"><Aleatorio min={1} max={60} /></Card>
+      <Card titulo="#3 - Fragmento"><Fragmento /></Card>
+      <Card titulo="#2 - Com Parâmetro"><ComParametro titulo="Situação do Aluno" aluno="Pedro" nota={9.3}  /></Card>
+      <Card titulo="#01 - Primeiro Componente"><Primeiro /></Card>
+      
+      
+      
     </div>
   );
 }
---
+++ fundamentos-react/src/components/basicos/Fragmento.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Fragmento = () => {
+  return (
+    <div>
+      <h1>Fragmento</h1>
+        <p>Cuidado com esse erro!</p>
+    </div>
+  );
+};
+
+export default Fragmento;
