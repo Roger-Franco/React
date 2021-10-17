89f771d7d55bf7f56d9cc78b160ea88c80c3b5aa
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-17 17:34:15 -0400

    condicional par ou impar

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 
 import Card from "./components/layout/Card";
 
+import ParOuImpar from "./components/condicional/ParOuImpar";
 import TabelaProdutos from "./components/repeticao/TabelaProdutos";
 import ListaAlunos from "./components/repeticao/ListaAlunos";
 import Familia from "./components/basicos/Familia";
@@ -19,6 +20,9 @@ export default () => {
     <div className="App">
       <h1>Fundamentos React</h1>
       <div className="Cards">
+        <Card titulo="#08 - Renderezição Condicional" color="#982395">
+          <ParOuImpar numero={20}></ParOuImpar>
+        </Card>
         <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
           <TabelaProdutos></TabelaProdutos>
         </Card>
