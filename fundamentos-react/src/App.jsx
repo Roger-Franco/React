42e3303baf428bec21ca4faa2828effcf675b2a1
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-21 10:40:54 -0400

    Minha tentativa para os numeros da megasena

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import "./App.css";
+
+import Megasena from "./components/mega/Mega";
 import Contador from "./components/contador/Contador"
 import Card from "./components/layout/Card";
 import Input from './components/formulario/Input'
@@ -23,6 +25,9 @@ export default () => {
     <div className="App">
       <h1>Fundamentos React</h1>
       <div className="Cards">
+        <Card titulo="#13 - Megasena" color="#424242">
+          <Megasena />
+        </Card>
         <Card titulo="#12 - Contador" color="#424242">
           <Contador numeroInicial={10}></Contador>
         </Card>
